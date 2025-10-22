b46b747fbea3266adef6cbce3191dffc2f62fc65
ilham-2001 <arizqyakbar@gmail.com>
2025-10-22 19:27:17 +0700

    add building block component main card for transactions and topup

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,9 +1,13 @@
 import Header from '../components/Header'
+import MainCard from '../components/MainCard'
 
 const Transactions = () => {
   return (
     <div>
       <Header />
+        <div className='container mx-auto flex flex-col gap-12 mt-8'>
+          <MainCard />
+      </div>
     </div>
   )
 }
